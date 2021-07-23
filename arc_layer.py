d1681ad4a84d3da4d7d54c021dc28c497598cf40
Dor Goldenberg <dorgoldenberg@gmail.com>
2021-07-23 10:37:30 +0300

    add centrality measures

--- arc_layer.py
+++ arc_layer.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
+import graph_aspects
 
 path = "https://raw.githubusercontent.com/dorgol/citation_pattern/main/affiliations.csv"
 affiliations = pd.read_csv(path)
@@ -41,11 +42,14 @@ GREEN_RGB = [0, 255, 0, 40]
 RED_RGB = [240, 100, 0, 40]
 
 
-def create_maps(year, min_num, type_num, scaling):
+
+def create_maps(year, min_num, type_num, scaling, method, quantile):
     # Specify a deck.gl ArcLayer
+    G = graph_aspects.create_yearly_graph(year, year)
+    data = graph_aspects.filtered_graph(year, G, method, quantile, min_num)
     arc_layer = pdk.Layer(
         "ArcLayer",
-        data=merge_loc(year, min_num),
+        data=data,
         get_width= type_num + "/" + str(scaling),
         get_source_position=["Longitude_x", "Latitude_x"],
         get_target_position=["Longitude_y", "Latitude_y"],
@@ -60,7 +64,7 @@ def create_maps(year, min_num, type_num, scaling):
                          longitude=-98.030561,
                          pitch=50,
                          zoom=3)
-    TOOLTIP_TEXT = {"html": "{" + type_num + "}"  + "citations  <br /> {DisplayName_x} citing - {DisplayName_y} cited <br /> "
+    TOOLTIP_TEXT = {"html": "{" + type_num + "}"  + " citations. Centrality of citing institution: {centrality}  <br /> {DisplayName_x} citing - {DisplayName_y} cited <br /> "
                         "Citing institution in red; Cited institution in green"}
     # deck = pdk.Deck(arc_layer, initial_view_state=view, tooltip=TOOLTIP_TEXT)
     st.title("Pattern of Citations Between Institutions")
@@ -70,6 +74,11 @@ year = st.sidebar.slider('year', 1990, 1999, 1990, 1)
 min_num = st.sidebar.slider('minimum citations', 5, 300, 10, 10)
 type_num = st.sidebar.selectbox('type', ['type_1', 'type_2', 'type_3', 'type_4'])
 scaling = st.sidebar.slider('line width scaler', 10, 1000, 100, 10)
-create_maps(type_num=type_num, year=year, min_num=min_num, scaling=scaling)
+method = st.sidebar.selectbox('method', ['degree_centrality', 'in_degree_centrality', 'out_degree_centrality',
+ 'eigenvector_centrality', 'closeness_centrality', 'voterank', 'betweenness_centrality',
+ 'edge_betweenness_centrality', 'harmonic_centrality']
+)
+quantile = st.sidebar.slider('quantile', 0.05, 1.0, 0.5, 0.05)
+create_maps(type_num=type_num, year=year, min_num=min_num, scaling=scaling, method=method, quantile=quantile)
 
 # r.to_html("arc_layer.html")
--- graph_aspects.py
+++ graph_aspects.py
@@ -1,11 +1,40 @@
 import networkx as nx
 import pandas as pd
-from arc_layer import merge_loc
-from networkx.algorithms import approximation
-
 
 path = "https://raw.githubusercontent.com/dorgol/citation_pattern/main/affiliations.csv"
 affiliations = pd.read_csv(path)
+
+def read_yearly_citations(year):
+    #Goal: read the citations data in a given year
+    #Gets: year; int. the desired year of the data.
+    #Returns: dataframe. Read 'Goal'.
+    return pd.read_csv('https://raw.githubusercontent.com/dorgol/citation_pattern/main/citations_yearly/citations_yearly' +
+                       str(year) + '.csv')
+
+def merge_loc(year, min_num, type_num = 'type_2'):
+    #Goal: merge the citations data with a location of every institution.
+    #Gets: year; int. The desired year of the data.
+    #      min_num; int. The minimal number of citations that will be included in the data.
+    #      type_num: str; either 'type_1', 'type_2', 'type_3' or 'type_4'. The type of citation.
+    #      For more information read the description in 'citations.citation_amount'.
+    #Returns: dataframe; read the 'Goal'.
+    df = read_yearly_citations(year)
+    a = pd.merge(df, affiliations, 'inner',
+             left_on='CitingAffiliatoinId',
+             right_on='AffiliationId',
+             left_index=False,
+             right_index=False
+             )
+    a = a.dropna()
+    b = pd.merge(a, affiliations, 'inner',
+             left_on='CitedAffiliatoinId',
+             right_on='AffiliationId',
+             left_index=False,
+             right_index=False
+             )
+    b = b.dropna()
+    b = b[b[type_num] >= min_num]
+    return b
 
 def loc_all_years(start_year = 1990, end_year = 1999, min_num = 0, type_num = 'type_2'):
     years_range = range(start_year,end_year+1)
@@ -27,11 +56,10 @@ def create_yearly_graph(start_year, end_year, type_num = 'type_2'):
     nx.set_node_attributes(G, aff)
     return G
 
-g90 = create_yearly_graph(1990, 1990)
-
 #centrality
 def graph_centrality(G, method, **kwargs):
-    centrality = method(G, **kwargs)
+    func = eval('nx.' + method)
+    centrality = func(G, **kwargs)
     if type(centrality) != list:
         centrality = sorted(((c, v) for v, c in centrality.items()), reverse=True)
     else:
@@ -39,42 +67,18 @@ def graph_centrality(G, method, **kwargs):
     centrality = pd.DataFrame(centrality, columns=['centrality', 'AffiliationId'])
     return centrality
 
-def filtered_graph(year, G, method, quantile, type_num = 'type_2'):
-    data = merge_loc(year, 0, type_num)
+def filtered_graph(year, G, method, quantile, min_num, type_num = 'type_2'):
+    data = merge_loc(year, min_num, type_num)
     centrality = graph_centrality(G, method)
     data = pd.merge(data, centrality, left_on='CitingAffiliatoinId',right_on ='AffiliationId')
-    data = data[data.centrality < data.centrality.quantile(quantile)]
+    data = data[data.centrality > data.centrality.quantile(quantile)]
     return data
-
-filtered_graph(1990, g90, nx.in_degree_centrality, 0.95)
-
-a = graph_centrality(g90,nx.in_degree_centrality)
 
 
 # no_weight = ['degree_centrality', 'in_degree_centrality', 'out_degree_centrality',
 #  'eigenvector_centrality', 'closeness_centrality', 'voterank']
 # yes_weight = ['betweenness_centrality', 'edge_betweenness_centrality', 'harmonic_centrality']
 
-
-
-
-
-
-
-
-
-
-
-
 #TODO: community
 #TODO: cliques
 #TODO: clustering
-
-
-
-approximation.max_clique(g90)
-approximation.average_clustering(g90)
-nx.degree_assortativity_coefficient(g90, x='in', y='out', weight='type_2')
-nx.degree_pearson_correlation_coefficient(g90, weight='type_2')
-
-
